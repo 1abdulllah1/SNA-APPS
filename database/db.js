8b22aba69ce576ca0bf036415f4c883adf893889
1abdullah1 <ishola2023m@gmail.com>
2025-06-19 18:21:06 +0100

    final improved one

--- database/db.js
+++ database/db.js
@@ -1,15 +1,12 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false  // Required for connecting to hosted DB like Render
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
-
-pool.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL database'))
-  .catch(err => console.error('❌ Database connection error:', err));
 
 module.exports = pool;
